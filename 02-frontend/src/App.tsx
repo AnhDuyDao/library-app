df4700e0825ed05e81f3e75acc7234607091f950
DuyAnh <duyanhdao297@gmail.com>
2025-02-13 15:35:57 +0700

    Routing home and search

--- 02-frontend/src/App.tsx
+++ 02-frontend/src/App.tsx
@@ -2,13 +2,28 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { SearchBookPage } from './layouts/SearchBookPage/SearchBookPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { HomePage } from './layouts/HomePage/HomePage';
 
 export const App = () => {
   return (
-    <div>
+    <div className='d-flex flex-column min-vh-100'>
       <Navbar />
-      {/* <HomePage /> */}
-      <SearchBookPage />
+      <div className='flex-grow-1'>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+
+          <Route path="/home">
+            <HomePage />
+          </Route>
+
+          <Route path="/search">
+            <SearchBookPage />
+          </Route>
+        </Switch>
+      </div>
       <Footer />
     </div>
 
