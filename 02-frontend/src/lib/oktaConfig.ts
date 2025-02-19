5414b0517b2665e09c612de60617fa27128f02b3
DuyAnh <duyanhdao297@gmail.com>
2025-02-20 01:04:05 +0700

    Login and logout with okta

---
+++ 02-frontend/src/lib/oktaConfig.ts
@@ -0,0 +1,9 @@
+export const oktaConfig = {
+    clientId: '0oang9c05pmeVuFQ35d7',
+    issuer: 'https://dev-15866290.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true,
+    language: "en",
+}
