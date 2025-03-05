3b961d547956fe5ba0c46df1483baf0cd193de0d
Duy Anh <duyanhdao297@gmail.com>
2025-03-05 14:38:23 +0700

    Admin question req and update message

--- 01-backend/spring-boot-library/src/main/java/com/duyanh/spring_boot_library/controller/MessagesController.java
+++ 01-backend/spring-boot-library/src/main/java/com/duyanh/spring_boot_library/controller/MessagesController.java
@@ -1,6 +1,7 @@
 package com.duyanh.spring_boot_library.controller;
 
 import com.duyanh.spring_boot_library.entity.Message;
+import com.duyanh.spring_boot_library.requestmodels.AdminQuestionRequest;
 import com.duyanh.spring_boot_library.service.MessagesService;
 import com.duyanh.spring_boot_library.utils.ExtractJWT;
 import org.springframework.web.bind.annotation.*;
@@ -22,4 +23,14 @@ public class MessagesController {
         messagesService.postMessage(messageRequest, userEmail);
     }
 
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization") String token,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "sub");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "userType");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Adminstration page only.");
+        }
+        messagesService.putMessage(adminQuestionRequest, userEmail);
+    }
 }
--- 01-backend/spring-boot-library/src/main/java/com/duyanh/spring_boot_library/service/MessagesService.java
+++ 01-backend/spring-boot-library/src/main/java/com/duyanh/spring_boot_library/service/MessagesService.java
@@ -2,8 +2,11 @@ package com.duyanh.spring_boot_library.service;
 
 import com.duyanh.spring_boot_library.dao.MessageRepository;
 import com.duyanh.spring_boot_library.entity.Message;
+import com.duyanh.spring_boot_library.requestmodels.AdminQuestionRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -19,5 +22,17 @@ public class MessagesService {
         Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
         message.setUserEmail(userEmail);
         messageRepository.save(message);
+    }
+
+    public void putMessage(AdminQuestionRequest adminQuestionRequest, String userEmail) throws Exception {
+        Optional<Message> message = messageRepository.findById(adminQuestionRequest.getId());
+        if (!message.isPresent()) {
+            throw new Exception("Message not found");
+        }
+
+        message.get().setAdminEmail(userEmail);
+        message.get().setResponse(adminQuestionRequest.getResponse());
+        message.get().setClosed(true);
+        messageRepository.save(message.get());
     }
 }
