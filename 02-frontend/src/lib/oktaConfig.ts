214db0c129817d087fe806e3a84f7c011cc0858f
Duy Anh <duyanhdao297@gmail.com>
2025-03-31 15:16:58 +0700

    React URL env

--- 02-frontend/src/lib/oktaConfig.ts
+++ 02-frontend/src/lib/oktaConfig.ts
@@ -3,7 +3,7 @@ import { features } from "process";
 export const oktaConfig = {
     clientId: '0oang9c05pmeVuFQ35d7',
     issuer: 'https://dev-15866290.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: `${process.env.REACT_APP_URL}/login/callback`,
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
