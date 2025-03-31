c0f591ff89d538c7ca52972b459bd87eb2605e07
Duy Anh <duyanhdao297@gmail.com>
2025-04-01 00:21:51 +0700

    Update Readme file

--- 02-frontend/src/lib/oktaConfig.ts
+++ 02-frontend/src/lib/oktaConfig.ts
@@ -1,8 +1,8 @@
 import { features } from "process";
 
 export const oktaConfig = {
-    clientId: '0oang9c05pmeVuFQ35d7',
-    issuer: 'https://dev-15866290.okta.com/oauth2/default',
+    clientId: `${process.env.REACT_APP_OKTA_CLIENT_ID}`,
+    issuer: `https://${process.env.REACT_APP_OKTA_ISSUER}/oauth2/default`,
     redirectUri: `${process.env.REACT_APP_URL}/login/callback`,
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
